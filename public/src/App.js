109e930a49ef595958135fe9730097e6ba1a91c2
Chozenalpha <ronaldowino716@gmail.com>
2025-12-18 14:52:33 +0300

    Create App component for Ronald Trading App

---
+++ public/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Ronald Trading App</h1>
+      <p>Connecting Maseno traders and buyers in one place.</p>
+    </div>
+  );
+}
+
+export default App;
